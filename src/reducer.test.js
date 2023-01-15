54e8c7f9643262b654bc9d53a98dcadf6189e513
Tino <tino.teittinen@edu.turkuamk.fi>
2023-01-15 22:15:15 +0200

    some tests added to 6.1

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -33,4 +33,49 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+  test('resetting states works', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    const filledState = {
+      good: 2,
+      ok: 3,
+      bad: 2
+    }
+    deepFreeze(filledState)
+    const newState = counterReducer(filledState, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
 })
